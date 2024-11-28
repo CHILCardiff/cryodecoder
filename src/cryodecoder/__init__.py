71aeb6fda427d728e19b0b7b0c6d385bf30f33ba
lisa-craw <lisa.craw@utas.edu.au>
2024-11-28 17:59:51 +1300

    fill in decoding for get_instrument_packet() W2 and C1

--- src/cryodecoder/__init__.py
+++ src/cryodecoder/__init__.py
@@ -5,11 +5,9 @@ import datetime
 import importlib.resources
 import os
 import cryodecoder
+import struct
 
-#load in info from packets.toml
-
-# JH: can't rely on "packets.toml" being on the executable path, 
-#     so we need to use importlib.resources to load the TOML config
+#region load in info from packets.toml
 
 # Create empty packets object
 packets = None
@@ -27,6 +25,7 @@ if packets == None:
 
 packet_types_string = list(packets['InstrumentType'].keys())
 packet_types_bytes = [bytes(item, encoding='utf-8') for item in packet_types_string]
+#endregion
 
 def bytes_to_packets(byte_array):
     """
@@ -38,7 +37,6 @@ def bytes_to_packets(byte_array):
             if byte_array[i:i+2]==packet_types_bytes[j]:
                 sd_packets.append(byte_array[i:i+(packets['InstrumentType'][packet_types_string[j]]['length'])])
     return sd_packets  
-
 
 # Instrument packets
 class InstrumentPacket:
@@ -68,16 +66,16 @@ class CryoeggPacket(InstrumentPacket):
     temperature_raw         : int
     battery_voltage         : int
     sequence_number         : int
-    rssi                    : float
-#    packet_version          : str
+#    rssi                    : float
+    packet_type          : str
     # Database-only: generated when retrieving from cryodb
     cryoegg_raw_id      : int = None,
     receiver_data_id    : int = None
     ingest_id           : int = None
     
-    # Implement get_identifier from packet_version
+    # Implement get_identifier from packet_type
     def get_identifier(self):
-        return self.packet_version
+        return self.packet_type
         
 
 @dataclass
@@ -104,17 +102,14 @@ class CryowurstPacket(InstrumentPacket):
     temperature_keller_raw  : int
     battery_voltage         : int
     sequence_number         : int
-    rssi                    : float
-#    packet_version          : str 
+#    rssi                    : float
+    packet_type: str 
     # Database-only: generated when retrieving from cryodb
     receiver_data_id    : int = None
     ingest_id           : int = None
 
     def get_identifier(self) -> str:
-        return self.packet_version
-    def decode(self):
-#        self.temperature_tmp117_raw=int.from_bytes(self.raw_data[])
-        pass
+        return self.packet_type
         
 
 @dataclass
@@ -153,26 +148,70 @@ class DataPacket:
         else:
             print('This isn\'t a packet type I recognise...')
     @abstractmethod
+    @abstractmethod
+    def get_raw(self) -> bytes:
+        pass
+
+class SDPacket(DataPacket):
+    def get_raw(self):
+        """returns raw data of complete SDPacket
+        """
+        return self.raw_data
+    def get_receiver_packet(self) -> ReceiverPacket:
+        """extracts and decodes receiver packet for SD packet"""
+        #work in progress
+        receiver_packet_end = packets['InstrumentType'][self.packet_type.decode('utf-8')]['instrument_packet_start']
+        instrument_packet_end = packets['InstrumentType'][self.packet_type.decode('utf-8')]['instrument_packet_length']+receiver_packet_end
+        # Get raw bytes for receiver packet
+        receiver_packet_raw=self.raw_data[0:receiver_packet_end]+self.raw_data[instrument_packet_end:-1]
+        # create receiver packet
+        packet_info=packets['ReceiverPacket']
+        timestamp_unix = int.from_bytes(receiver_packet_raw[packet_info['timestamp']['start_index']:packet_info['timestamp']['end_index']+1], 
+                                    byteorder='little') #TODO: convert from Unix timestamp to datetime
+        timestamp=datetime.datetime.fromtimestamp(timestamp_unix)
+        voltage = int.from_bytes(receiver_packet_raw[packet_info['logger_voltage']['start_index']:packet_info['logger_voltage']['end_index']+1],
+                                   byteorder='little')
+        [temperature] = struct.unpack('f',receiver_packet_raw[packet_info['logger_temp']['start_index']:packet_info['logger_temp']['end_index']+1]) 
+#            pressure= struct.unpack('f',receiver_packet_raw[packet_info['logger_pressure']['start_index']:packet_info['logger_pressure']['end_index']+1]) 
+        pressure= int.from_bytes(receiver_packet_raw[packet_info['logger_pressure']['start_index']:packet_info['logger_pressure']['end_index']+1], 
+                                    byteorder = 'little', signed=True)
+        channel= int.from_bytes(receiver_packet_raw[packet_info['channel_number']['start_index']:packet_info['channel_number']['end_index']+1], 
+                                    byteorder = 'little', signed=False)
+        receiver_packet = ReceiverPacket(timestamp=timestamp,
+                                         voltage=voltage,
+                                         temperature=temperature,
+                                         pressure=pressure,
+                                         channel=channel
+        )
+        self.instrument_type= receiver_packet_raw[0:2].decode('utf-8')
+        #self.time_formatted = datetime.datetime.fromtimestamp(self.time_int)
+#        self.logger_temp = int.from_bytes(receiver_packet_raw[6:10], byteorder='little')
+        # Sequence number goes to the instrument packet 
+        # self.sequence_number = int.from_bytes(receiver_packet_raw[-2:-1], byteorder='little')
+        return receiver_packet
     def get_instrument_packet(self) -> InstrumentPacket:
         """implements instrument packet for SD packet"""
         #work in progress
         packet_start = packets['InstrumentType'][self.packet_type.decode('utf-8')]['instrument_packet_start']
         packet_end = packets['InstrumentType'][self.packet_type.decode('utf-8')]['instrument_packet_length']+packet_start
         instrument_packet = self.raw_data[packet_start:packet_end]
-        #should do something clever with the packet types dictionary here
+        #check which packet type we have, and then decode accordingly 
         if self.packet_type==b'C1':
             packet_info = packets['CryoeggPacket']
             instrument_id=0
             conductivity_raw=int.from_bytes(instrument_packet[packet_info['conductivity']['start_index']:packet_info['conductivity']['end_index']],
                                         byteorder = 'little', signed=False)
-            #TODO obvs will come back and fill these in
-            temperature_pt1000_raw = 0
-            pressure_raw=0
-            temperature_raw=0
-            battery_voltage=0
-            sequence_number=0
-            rssi=0
-            packet_version=0
+            temperature_pt1000_raw = int.from_bytes(instrument_packet[packet_info['temperature_pt1000']['start_index']:packet_info['temperature_pt1000']['end_index']+1],
+                                        byteorder = 'little', signed=False)
+            pressure_raw = int.from_bytes(instrument_packet[packet_info['pressure']['start_index']:packet_info['pressure']['end_index']+1],
+                                        byteorder = 'little', signed=False)
+            temperature_raw = int.from_bytes(instrument_packet[packet_info['temperature']['start_index']:packet_info['temperature']['end_index']+1],
+                                        byteorder = 'little', signed=True)
+            battery_voltage = int.from_bytes(instrument_packet[packet_info['battery_voltage']['start_index']:packet_info['battery_voltage']['end_index']+1], byteorder = 'little', signed=True)
+            sequence_number = int.from_bytes(instrument_packet[packet_info['sequence_number']['start_index']:packet_info['sequence_number']['end_index']+1],
+                                        byteorder = 'little', signed=True)
+         #   rssi=0
+            packet_type=self.packet_type
             return CryoeggPacket(instrument_id=instrument_id,
                                  conductivity_raw=conductivity_raw,
                                  temperature_pt1000_raw=temperature_pt1000_raw,
@@ -180,31 +219,47 @@ class DataPacket:
                                  temperature_raw=temperature_raw, 
                                  battery_voltage=battery_voltage,
                                  sequence_number=sequence_number,
-                                 rssi=rssi
-#                                 packet_version=packet_version
+                                 #rssi=rssi,
+                                 packet_type=packet_type
                                  )
         elif self.packet_type == b'W2':
             packet_info = packets['CryowurstPacket']
+            #TODO instrument id
             instrument_id=0
-            temperature_tmp117_raw=int.from_bytes(instrument_packet[packet_info['temperature']['start_index']:packet_info['temperature']['end_index']],
+            temperature_tmp117_raw=int.from_bytes(instrument_packet[packet_info['temperature']['start_index']:packet_info['temperature']['end_index']+1],
                                          byteorder = 'little', signed=True)
-            mag_x_raw=0
-            mag_y_raw=0
-            mag_z_raw=0
-            accel_imu_x_raw=0
-            accel_imu_y_raw=0
-            accel_imu_z_raw=0
-            accel_tilt_x_raw=0
-            accel_tilt_y_raw=0
-            accel_tilt_z_raw=0
-            pitch_raw=0
-            roll_raw=0
-            conductivity_raw=0
-            pressure_raw=0
-            temperature_keller_raw=0
-            battery_voltage=0
-            sequence_number=0
-            rssi=0
+            mag_x_raw=int.from_bytes(instrument_packet[packet_info['magnetometer_x']['start_index']:packet_info['magnetometer_x']['end_index']+1],
+                                         byteorder = 'little', signed=True)
+            mag_y_raw=int.from_bytes(instrument_packet[packet_info['magnetometer_y']['start_index']:packet_info['magnetometer_y']['end_index']+1],
+                                         byteorder = 'little', signed=True)
+            mag_z_raw=int.from_bytes(instrument_packet[packet_info['magnetometer_z']['start_index']:packet_info['magnetometer_z']['end_index']+1],
+                                         byteorder = 'little', signed=True)
+            accel_imu_x_raw=int.from_bytes(instrument_packet[packet_info['accelerometer_x']['start_index']:packet_info['accelerometer_x']['end_index']+1],
+                                         byteorder = 'little', signed=True)
+            accel_imu_y_raw=int.from_bytes(instrument_packet[packet_info['accelerometer_y']['start_index']:packet_info['accelerometer_y']['end_index']+1],
+                                         byteorder = 'little', signed=True)
+            accel_imu_z_raw=int.from_bytes(instrument_packet[packet_info['accelerometer_z']['start_index']:packet_info['accelerometer_z']['end_index']+1],
+                                         byteorder = 'little', signed=True)
+            accel_tilt_x_raw=int.from_bytes(instrument_packet[packet_info['tilt_x']['start_index']:packet_info['tilt_x']['end_index']+1],
+                                         byteorder = 'little', signed=True)
+            accel_tilt_y_raw=int.from_bytes(instrument_packet[packet_info['tilt_y']['start_index']:packet_info['tilt_y']['end_index']+1],
+                                         byteorder = 'little', signed=True)
+            accel_tilt_z_raw=int.from_bytes(instrument_packet[packet_info['tilt_z']['start_index']:packet_info['tilt_z']['end_index']+1],
+                                         byteorder = 'little', signed=True)
+            pitch_raw=int.from_bytes(instrument_packet[packet_info['pitch_x']['start_index']:packet_info['pitch_x']['end_index']+1],
+                                         byteorder = 'little', signed=True)
+            roll_raw=int.from_bytes(instrument_packet[packet_info['roll_y']['start_index']:packet_info['roll_y']['end_index']+1],
+                                         byteorder = 'little', signed=True)
+            conductivity_raw=int.from_bytes(instrument_packet[packet_info['conductivity']['start_index']:packet_info['conductivity']['end_index']],
+                                        byteorder = 'little', signed=False)
+            pressure_raw = int.from_bytes(instrument_packet[packet_info['pressure']['start_index']:packet_info['pressure']['end_index']+1],
+                                        byteorder = 'little', signed=False)
+            temperature_keller_raw = int.from_bytes(instrument_packet[packet_info['keller_temperature']['start_index']:packet_info['keller_temperature']['end_index']+1],
+                                        byteorder = 'little', signed=True)
+            battery_voltage = int.from_bytes(instrument_packet[packet_info['battery_voltage']['start_index']:packet_info['battery_voltage']['end_index']+1], byteorder = 'little', signed=True)
+            sequence_number = int.from_bytes(instrument_packet[packet_info['sequence_number']['start_index']:packet_info['sequence_number']['end_index']+1],
+                                        byteorder = 'little', signed=True)
+            packet_type=self.packet_type
             return CryowurstPacket(instrument_id=instrument_id,
                                     temperature_tmp117_raw=temperature_tmp117_raw,
                                     mag_x_raw=mag_x_raw,
@@ -223,41 +278,10 @@ class DataPacket:
                                     temperature_keller_raw=temperature_keller_raw,
                                     battery_voltage=battery_voltage,
                                     sequence_number=sequence_number,
-                                    rssi=rssi
+                                    #rssi=rssi,
+                                    packet_type=packet_type
                                     )
 
-    @abstractmethod
-    def get_receiver_packet(self) -> ReceiverPacket:
-        """implements receiver packet for SD packet"""
-        #work in progress
-        receiver_packet_end = packets['InstrumentType'][self.packet_type.decode('utf-8')]['instrument_packet_start']
-        instrument_packet_end = packets['InstrumentType'][self.packet_type.decode('utf-8')]['instrument_packet_length']+receiver_packet_end
-        # Get raw bytes for receiver packet
-        receiver_packet_raw=self.raw_data[0:receiver_packet_end]+self.raw_data[instrument_packet_end:-1]
-        # create receiver packet
-        receiver_packet = ReceiverPacket(
-            timestamp = int.from_bytes(receiver_packet_raw[2:6], byteorder='little'), #TODO: convert from Unix timestamp to datetime
-            voltage = int.from_bytes(receiver_packet_raw[14:16], byteorder='little'),
-            temperature = 0, #TODO: assign temperature
-            pressure = 0, #TODO: assign pressure
-            channel = 0, #TODO: assign channel 
-        )
-        self.header = receiver_packet_raw[0:2].decode('utf-8')
-        #self.time_formatted = datetime.datetime.fromtimestamp(self.time_int)
-#        self.logger_temp = int.from_bytes(receiver_packet_raw[6:10], byteorder='little')
-        # Sequence number goes to the instrument packet 
-        # self.sequence_number = int.from_bytes(receiver_packet_raw[-2:-1], byteorder='little')
-        return receiver_packet
-    def get_raw(self) -> bytes:
-        pass
-
-class SDPacket(DataPacket):
-
-
-    def get_raw(self):
-        """returns raw data of complete SDPacket
-        """
-        return self.raw_data
 
 
 class LingoMOPacket:
